91137403a3fabed51fb8439fd18f0b37b9acce2f
Vadym Suslov <vsuslov2906@gmail.com>
2022-05-20 15:39:14 +0200

    v1.1

---
+++ index.1fde76bc.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",(()=>{"#menu"===window.location.hash?document.body.classList.add("body--with-menu"):document.body.classList.remove("body--with-menu")}));
+//# sourceMappingURL=index.1fde76bc.js.map
